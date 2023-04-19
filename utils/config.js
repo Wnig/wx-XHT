fd712526ba2b9eeb9481374c1afd6532f2ff6daa
Wnig <770225508@qq.com>
2023-04-19 15:39:58 +0800

    初始化

---
+++ utils/config.js
@@ -0,0 +1,8 @@
+const config = {
+  // "restUrl": "https://abc.smartmapdt.com/xht/", //测试
+  // "restUrl": "http://192.168.1.107:8080/xht/", //本地测试
+  "restUrl": "https://tour.xinhongt.com/xht/", //正式
+}
+
+
+export default config
